747abb3a7eb777675eb28aeba44b67aee3e6bcaa
Fabricio Catae <fabricio.catae@microsoft.com>
2015-12-30 19:00:31 -0200

    $.load

--- page.js
+++ page.js
@@ -5,13 +5,16 @@ var response;
 //    alert(r);    
 // });
 
-$.ajax({
-    url: 'teste.html',
-    type: 'GET',
-    dataType: 'html',
-    success: function(r) {
-        response = r;
-        alert(r);
-    }
-}
-);
+// $.ajax({
+//     url: 'teste.html',
+//     type: 'GET',
+//     dataType: 'html',
+//     success: function(r) {
+//         response = r;
+//         alert(r);
+//     }
+// });
+
+$('h1').load('teste.html', function(r) {
+    alert('completed');
+});
