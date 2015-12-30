92c9f008f4844d50fbe15d1bda58a038e6cf52b1
Fabricio Catae <fabricio.catae@microsoft.com>
2015-12-30 21:34:52 -0200

    webdriver

---
+++ app.js
@@ -0,0 +1,18 @@
+var webdriverio = require('webdriverio');
+var options = {
+    desiredCapabilities: {
+        //browserName: 'firefox'
+    },
+    //logLevel: 'verbose',
+    host: 'localhost', port: '17556', path: '/'
+};
+ 
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://www.google.com')
+    .title(function(err, res) {
+        console.log('Title was: ' + res.value);
+    })
+    .end();
+
